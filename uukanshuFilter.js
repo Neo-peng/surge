75ad1131c32f7f0837a0c7337e33fc65c0f079b3
peng <jp170518@gmail.com>
2021-06-04 19:43:02 +0800

    uukanshuFilter

--- uukanshuFilter.js
+++ uukanshuFilter.js
@@ -2,7 +2,7 @@ var body = $response.body;
 // console.log(re.test(body));
 // 过滤掉推荐书籍和下方的分享按钮
 var res = body.match(/<div class="(samebooks1|share)[\s\S]*?"[\s\S]*?>[\s\S]*?<\/div>/g)
-// console.log(typeof res)
+console.log(typeof res)
 if (res !== null) {
   for (let e of res) {
     console.log(e)
