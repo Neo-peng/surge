6e03c28c148cad8da6a00f599e4171d5cbdcb7e7
peng <jp170518@gmail.com>
2021-06-04 19:33:16 +0800

    uukanshuFilter

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
-let headers = $response.headers;
-headers['X-Modified-By'] = 'Surge';
-headers['X-Powered-By'] = 'Surge';
-$done();
+var body = $response.body;
+// console.log(re.test(body));
+var res = body.match(/<div class="(samebooks1|share)[\s\S]*?"[\s\S]*?>[\s\S]*?<\/div>/g)
+console.log(typeof res)
+if (res !== null) {
+  for (let e of res) {
+    console.log(e)
+    console.log("分割线")
+    body = body.replaceAll(e, "")
+  }
+} else {
+  console.log("no match")
+}
+// body = body.replaceAll(re, "");
+$done({body});
--- uukanshu.js
+++
@@ -1,4 +0,0 @@
-let headers = $response.headers;
-headers['X-Modified-By'] = 'Surge';
-headers['X-Powered-By'] = 'Surge';
-$done();
